401ef77d8d834a254bdb24e3559a88be707df5f4
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-10-25 19:09:27 +0530

    fix: write hello world to display

--- src/kmain.c
+++ src/kmain.c
@@ -1,30 +1,7 @@
-#define BLACK 0x00
-#define BLUE 0x01
-#define WHITE 0xff
-
-
-char* frame_buffer = (char*) 0x000B8000; // memory mapped address for the frame buffer;
-
-/**
- * @brief Writes to the frame buffer characters
- * 
- * @param index of the cell to be written to
- * @param character to be written onto the cell
- * @param foreground color of the cell
- * @param background color of the cell
- */
-void frame_buffer_write_cell(
-    unsigned int index,
-    char character,
-    unsigned char foreground,
-    unsigned char background) {
-    frame_buffer[index] = character;
-    frame_buffer[index + 1] = ((foreground & 0x0f) << 4) | (background & 0x0f);
-}
-
+#include "../includes/io.h"
 
 int sum_of_three(int a, int b, int c) {
-    frame_buffer_write_cell(10, 'H', BLUE, BLACK);
-
+    write("Hello world!", 13);
+    write("hey!", 4);
     return a+b+c;
 }
