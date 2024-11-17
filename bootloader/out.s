d40754797b4ce78e4b7ac161cf7c74ba613eba64
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-11-17 09:42:56 +0530

    feat: re sturcture directory

--- bootloader/out.s
+++
@@ -1,8 +0,0 @@
-    global outb
-    section .text:
-    align 4
-    outb:
-        mov al, [esp + 8]
-        mov dx, [esp + 4]
-        out dx, al
-        ret
---
+++ kernel/src/kernel.c
@@ -0,0 +1,12 @@
+//#include "../includes/io.h"
+
+/**
+ * @brief Entry point of the os.
+ * 
+ * @return int 
+ */
+int kmain() {
+    char h = 'a';
+    char b = 'b';
+    return h + b;
+}
