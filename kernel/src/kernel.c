e98ef027866a75e69540185b89deb6c758f98b17
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-12-20 23:29:12 +0530

    feat: use limine as bootloader

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,4 +1,30 @@
-#include <tty.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <limine.h>
+#include <string.h>
+
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+// place requests here
+
+static volatile LIMINE_BASE_REVISION(3);
+
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile LIMINE_REQUESTS_END_MARKER;
+
+
+// Halt and catch fire function.
+static void hcf(void) {
+    for (;;) {
+        asm ("hlt");
+    }
+}
 
 
 /**
@@ -7,8 +33,28 @@
  * @return int 
  */
 int kmain() {
-    terminal_init();
+    hcf();
 
-    terminal_write("Hello, world!", 13);
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t *fb_ptr = framebuffer->address;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffff00;
+    }
+
+    // We're done, just hang...
+    hcf();
     return 0;
 }
