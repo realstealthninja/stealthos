e03b4f66c813435a71fe8e04a5f4311e8adf934a
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-12-21 18:21:24 +0530

    feat: add psf partial psf support

---
+++ kernel/include/psf.h
@@ -0,0 +1,47 @@
+#ifndef PSF_H
+#define PSF_H
+
+#include <stdint.h>
+
+/**
+ * @brief Describes a psf1 font header
+ * @link https://en.wikipedia.org/wiki/PC_Screen_Font
+ */
+ // height == width
+struct psf1_header {
+    uint8_t magic[2];
+    uint8_t mode;
+    uint8_t glyph_size;
+} __attribute__((packed));
+
+/**
+ * @brief Describes a psf2 font header
+ * @link https://en.wikipedia.org/wiki/PC_Screen_Font
+ */
+struct psf2_header {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t header_size;
+    uint32_t flags;
+    uint32_t length;
+    uint32_t glyph_size;
+    uint32_t height;
+    uint32_t width;
+} __attribute__((packed));
+
+struct psf2_font {
+    struct psf2_header *header;
+    void *glyph_buffer;
+} __attribute__((packed));
+
+
+struct psf1_font {
+    struct psf1_header *header;
+    void *glyph_buffer;
+} __attribute__((packed));
+
+
+void map_to_unicode_psf1(struct psf1_font* font, uint16_t* unicode_table);
+void map_to_unicode_psf2(struct psf2_font* font, uint16_t* unicode_table);
+
+#endif // PSF_H
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <limine.h>
+#include <psf.h>
 #include <string.h>
 
 
@@ -10,8 +11,17 @@ static volatile LIMINE_REQUESTS_START_MARKER;
 
 static volatile LIMINE_BASE_REVISION(3);
 
+__attribute__((used, section(".requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+
+// ask for the modules
+__attribute__((used, section(".requests")))
+static volatile struct limine_module_request module_request = {
+    .id = LIMINE_MODULE_REQUEST,
     .revision = 0
 };
 
@@ -24,6 +34,24 @@ static void hcf(void) {
     for (;;) {
         asm ("hlt");
     }
+}
+
+
+
+struct limine_file* get_file(const char* path) {
+    struct limine_module_response *module_response = module_request.response;
+
+    if (module_response == NULL) {
+        hcf();
+    }
+
+    for(size_t i = 0; i < module_response->module_count; i++) {
+        if(strcmp(path, module_response->modules[i]->path) == 0) {
+            return module_response->modules[i];
+        }
+    }
+
+    return NULL;
 }
 
 
@@ -41,9 +69,39 @@ int kmain() {
 
     // Ensure we got a framebuffer.
     if (framebuffer_request.response == NULL
-     || framebuffer_request.response->framebuffer_count < 1) {
+        || framebuffer_request.response->framebuffer_count < 1) {
         hcf();
     }
+
+    // Ensure we have modules
+    if (module_request.response == NULL
+        || module_request.response->module_count < 1) {
+
+        hcf();
+    }
+
+    struct limine_file *font_file = get_file("boot/assets/fonts/zap-vga16.psf");
+
+    // check if we got the font
+    if (font_file == NULL) {
+        hcf();
+    }
+
+    // get the psf header
+    struct psf1_header *font_header = (struct psf1_header*) font_file->address;
+
+    // the magic for all psf1 files is 0x3604
+    if(font_header->magic[0] != 0x36 || font_header->magic[1] != 0x04 ) {
+        // font isnt psf1
+        // halt for now in the future check if its psf2
+        hcf();;
+    }
+
+    struct psf1_font font = {
+        font_header,
+        font_file->address + sizeof(struct psf1_header)
+    };
+
 
     // Fetch the first framebuffer.
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
@@ -53,6 +111,7 @@ int kmain() {
         volatile uint32_t *fb_ptr = framebuffer->address;
         fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffff00;
     }
+
 
     // We're done, just hang...
     hcf();
