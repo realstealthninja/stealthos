acd5c43b51449b6e0f5e032ca537093f4df00f1d
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-12-30 20:36:01 +0530

    lint: string.c

--- stdlib/src/string.c
+++ stdlib/src/string.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+
 void *memcpy(void *dest, const void *src, size_t n) {
     uint8_t *pdest = (uint8_t *)dest;
     const uint8_t *psrc = (const uint8_t *)src;
