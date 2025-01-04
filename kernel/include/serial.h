d93e218deddc646ebdc2d37d4b3fe86943eaf6ac
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-01-04 18:30:19 +0530

    doc: document serial.h

--- kernel/include/serial.h
+++ kernel/include/serial.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief serial.h
+ */
+
 #ifndef SERIAL_H
 #define SERIAL_H
 
@@ -17,22 +22,74 @@
 #define SERIAL_LINE_ENABLE_DLAB         0x80
 
 
+/**
+ * @brief configures baud rate of the serial
+ *
+ * @param com the port to be used
+ * @param divisor 11,500 is the default baud rate
+ */
 void serial_configure_baud_rate(uint16_t com, uint16_t divisor);
 
+
+/**
+ * @brief configures the line
+ *
+ * @param com the port to be configured
+ */
 void serial_configure_line(uint16_t com);
 
+
+/**
+ * @brief configures the buffer
+ *
+ * @param com the port to be configured
+ */
 void serial_configure_buffer(uint16_t com);
 
+
+/**
+ * @brief configures the modem
+ *
+ * @param com the port to be configured
+ */
 void serial_configure_modem(uint16_t com);
 
+/**
+ * @brief checks if everything has transimtted
+ *
+ * @param com the port toe be checked
+ * @return true if transmit is empty 
+ * @return false if transmit is not empty
+ */
 bool serial_is_transmit_fifo_empty(uint16_t com);
 
+/**
+ * @brief initialized the serial port 0x3F8
+ */
 void serial_init(void);
 
+
+/**
+ * @brief puts a character to serial
+ *
+ * @param c character to be sent
+ */
 void serial_putc(char c);
 
+/**
+ * @brief sends the given data of specified length to serial
+ *
+ * @param data the data to be sent
+ * @param len the length of the data
+ */
 void serial_write(char* data, size_t len);
 
+
+/**
+ * @brief sends a string terminated with \0
+ *
+ * @param data the string to be sent
+ */
 void serial_write_string(char* data);
 
 #endif // SERIAL_H
