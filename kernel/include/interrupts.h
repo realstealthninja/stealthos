5c16a4f2112154d7810eb866151b9eacf8a98d41
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-11-12 05:35:45 +0530

    fix: define write and read register

--- kernel/include/interrupts.h
+++ kernel/include/interrupts.h
@@ -37,14 +37,39 @@ struct interrupt_descriptor_table_register_t {
     uint64_t base;
 } __attribute__((packed));
 
+struct cpu_status_t {
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rbp;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
 
-__attribute__((noreturn))
-void exception_handler(void);
+    uint64_t vector;
+    uint64_t error_code;
+
+    uint64_t iret_rip;
+    uint64_t iret_cs;
+    uint64_t iret_flags;
+    uint64_t iret_rsp;
+    uint64_t iret_ss;
+};
+
+
+struct cpu_status_t* interrupt_dispatcher(struct cpu_status_t* context);
+
 
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags, uint8_t ist);
 void idt_install_irq_handler(int8_t vector, void* handler);
-
-extern void* isr_stub_table[];
 
 void idt_init(void);
 
--- kernel/src/utils.c
+++ kernel/src/utils.c
@@ -8,3 +8,16 @@ void cpu_set_msr(uint32_t msr, uint32_t low, uint32_t high) {
 void cpu_get_msr(uint32_t msr, uint32_t* low, uint32_t* high) {
     asm volatile("rdmsr":  "=a"(*low), "=d"(*high): "c" (msr));
 }
+
+
+uint64_t read_register(uint32_t reg) {
+    uint32_t low;
+    uint32_t high;
+    cpu_get_msr(reg, &low, &high);
+
+    return ((uint64_t)high << 32) | low;
+}
+
+void write_register(uint32_t reg, uint64_t value) {
+    cpu_set_msr(reg, value & 0xffffffff00000000,value >> 32);
+}
